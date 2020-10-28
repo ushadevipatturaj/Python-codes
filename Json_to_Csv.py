6df29407e0e923c817a203f46f32f8a01a040c08
ushapatturaj <ushadevipatturaj@gmail.com>
2020-10-28 12:12:48 +0100

    json to python conversion

---
+++ Json_to_Csv.py
@@ -0,0 +1,36 @@
+import json
+import csv
+
+with open('sample_json_2.json', 'r') as json_file:
+    json_data = json.load(json_file)
+
+book_data = json_data['glossary']
+csv_writer = open('created_csv.csv', 'w')
+csv_data = csv.writer(csv_writer)
+count = 0
+for book in book_data:
+    if count == 0:
+        csv_data.writerow(book.keys())
+        csv_data.writerow(book.values())
+        count += 1
+    else:
+        csv_data.writerow(book.values())
+
+csv_writer.close()
+
+# with open("sample_json.json", "r") as json_file:
+#     json_data = json.load(json_file)
+#
+# book_data = json_data['Book_Details']
+# csv_writer = open("created_csv.csv", "w")
+# csv_data = csv.writer(csv_writer)
+#
+# count = 0
+# for book in book_data:
+#     if count == 0:
+#         csv_data.writerow(book.keys())
+#         count += 1
+#     else:
+#         csv_data.writerow(book.values())
+#
+# csv_writer.close()
